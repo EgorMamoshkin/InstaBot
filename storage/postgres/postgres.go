fe4a4d71ec9693b73320ca05e4b6b044e56113ac
EgorMamoshkin <mamoshkin.e@gmail.com>
2022-10-10 15:28:22 +0300

    Refactor. Postgres storage replaced to new package postgres,files storage recovered

---
+++ storage/postgres/postgres.go
@@ -0,0 +1,108 @@
+package postgres
+
+import (
+	"InstaBot/lib/er"
+	"InstaBot/storage"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"github.com/Davincible/goinsta/v3"
+)
+
+package files
+
+import (
+"InstaBot/lib/er"
+"InstaBot/storage"
+"context"
+"database/sql"
+"encoding/json"
+"github.com/Davincible/goinsta/v3"
+_ "github.com/jackc/pgx/v5"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+// New creates new PostgresSQL storage.
+func New(dsn string) (*Storage, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, er.Wrap("can't open database: ", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, er.Wrap("can't connect to database: ", err)
+	}
+	return &Storage{db: db}, nil
+}
+
+//SaveAccount Saves account in storage.
+func (s *Storage) SaveAccount(ctx context.Context, user *storage.User, username string) error {
+
+	config := user.InstAcc.ExportConfig()
+	configJson, err := json.Marshal(config)
+	instConfig := string(configJson)
+	if err != nil {
+		return er.Wrap("can't convert config into json:", err)
+	}
+
+	q := `INSERT INTO instagram_users (username_tg, instagram_acc, last_post_id) VALUES ($1, $2, $3)`
+	_, err = s.db.ExecContext(ctx, q, username, instConfig, user.LastPostID)
+	if err != nil {
+		return er.Wrap("can't save account data into database: ", err)
+	}
+	return nil
+}
+
+//GetAccount imports account from storage.
+func (s *Storage) GetAccount(ctx context.Context, username string) (*storage.User, error) {
+
+	q := `SELECT instagram_acc, last_post_id FROM instagram_users WHERE username_tg = $1`
+
+	row := s.db.QueryRowContext(ctx, q, username)
+
+	var instConfig string
+	var lastPostID string
+
+	err := row.Scan(&instConfig, &lastPostID)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, er.Wrap("can't get instagram account: ", err)
+	}
+
+	config := goinsta.ConfigFile{}
+
+	err = json.Unmarshal([]byte(instConfig), &config)
+	if err != nil {
+		return nil, er.Wrap("can't convert json to ConfigFile: ", err)
+	}
+
+	instAcc, err := goinsta.ImportConfig(config)
+	if err != nil {
+		return nil, er.Wrap("can't import instagram account: ", err)
+	}
+
+	return &storage.User{LastPostID: lastPostID, InstAcc: instAcc}, nil
+}
+
+// SaveLastPostID saves last post from feed in storage.
+func (s *Storage) SaveLastPostID(ctx context.Context, postID string, username string) error {
+	q := `UPDATE instagram_users SET last_post_id = $1 WHERE username_tg = $2`
+
+	_, err := s.db.ExecContext(ctx, q, postID, username)
+	if err == sql.ErrNoRows {
+		return er.Wrap("can't save last post ID: ", err)
+	}
+	return nil
+}
+
+func (s *Storage) Init(ctx context.Context) error {
+	q := `CREATE TABLE IF NOT EXISTS instagram_users (user_id INT PRIMARY KEY, username_tg VARCHAR, instagram_acc VARCHAR, last_post_id VARCHAR)`
+	_, err := s.db.ExecContext(ctx, q)
+	if err != nil {
+		return er.Wrap("can't create new table: ", err)
+	}
+}
