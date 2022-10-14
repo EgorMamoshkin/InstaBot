741a5024b717704c8a7fc6d9133916c66303feaf
EgorMamoshkin <mamoshkin.e@gmail.com>
2022-10-14 18:45:05 +0300

    Refactor. Auth server. handler package deleted

--- auth/authserver/authserver.go
+++ auth/authserver/authserver.go
@@ -1,23 +1,25 @@
 package authserver
 
 import (
-	"github.com/EgorMamoshkin/InstaBot/auth/handler"
+	"fmt"
 	"github.com/EgorMamoshkin/InstaBot/lib/er"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type AuthServer struct {
-	handler *handler.ResponseHandler
+	router *mux.Router
 }
 
-func New(handler *handler.ResponseHandler) *AuthServer {
-	return &AuthServer{handler: handler}
+func New(router *mux.Router) *AuthServer {
+	return &AuthServer{router: router}
 }
 
 func (a *AuthServer) StartLS() error {
 	r := mux.NewRouter()
-	r.HandleFunc("/auth", a.handler.TokenRedirect)
+	r.HandleFunc("/auth", RedirectToken)
 
 	http.Handle("/", r)
 
@@ -27,4 +29,15 @@ func (a *AuthServer) StartLS() error {
 	}
 
 	return nil
+}
+
+func RedirectToken(w http.ResponseWriter, r *http.Request) {
+	code := strings.TrimRight(r.URL.Query().Get("code"), "#_")
+	if code == "" {
+		log.Println("there are no code")
+	}
+
+	link := fmt.Sprintf("https://telegram.me/share/url?url=/getAccess&text=%s", code)
+
+	http.Redirect(w, r, link, http.StatusSeeOther)
 }
